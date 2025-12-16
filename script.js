7c8f7b7f8110fa45fdd92a0d8d6b3446f164dead
Noah Gullo <noahgulloingram@gmail.com>
2025-12-16 15:41:39 -0800

    Add ability to create/delete grid using buttons

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const container = document.querySelector(".container");
+let squares = undefined;
+let gridSize = 0;
+
+const newGridButton = document.querySelector(".new-grid");
+const clearButton = document.querySelector(".clear-grid");
 
 function createGrid(size){
+    deleteGrid();
+    gridSize = size;
     height = Math.floor(parseInt(container.getAttribute("width"))) / size;
     width = height;
 
@@ -8,14 +15,27 @@ function createGrid(size){
         for(let j = 0; j < size; j++)
         {
             const square = document.createElement("div");
+            square.classList.add("square");
             square.style.display = "flex";
             square.setAttribute("style",`height: ${height}px; width: ${width}px`);
-            square.style.backgroundColor = "white";
+            if((i + j) % 2 == 0 ){
+                square.style.backgroundColor = "pink";
+            }
             square.style.flexShrink = "0"; 
             container.appendChild(square);
         }
-        
+    }
+
+    squares = document.querySelectorAll(".square");
+}
+
+function deleteGrid(){
+    squares = document.querySelectorAll(".square");
+    for(let i = 0; i < squares.length; i++){
+        let square = squares[i];
+        container.removeChild(square);
     }
 }
 
-createGrid(5);
+clearButton.addEventListener("click", () => deleteGrid());
+newGridButton.addEventListener("click", () => createGrid(parseInt(prompt("Num 1-100"))));
